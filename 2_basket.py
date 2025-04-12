6356146f3df5b8ef89e7d673346c958cab6d76ab
vitarafavich <v.rafaevich@g.nsu.ru>
2025-04-12 17:40:55 +0700

    second

--- 2_basket.py
+++ 2_basket.py
@@ -5,6 +5,8 @@ class Basket:
     Class representing a marketplace basket
     """
     all_stuff = {}
+
+
     def __init__(self):
         self.__base = []
 
@@ -23,12 +25,14 @@ class Basket:
 
     @classmethod
     def product_info(cls, file):
-        with open ('product_data.txt', 'r', encoding='utf8') as f:
+        with open(file, 'r', encoding='utf8') as f:
             for line in f:
                 name, code, info, price = line.strip().split('; ')
                 result = Product(name, code.strip(), info, price)
                 #calling the class and its atribute all_stuff. key - name of the product, value - other info. price has ind 4
-                cls.all_stuff[result.repr()[0]] = result.repr()[1:]
+                cls.all_stuff[result.name] = [result.code, result.country, result.info, result.price]
+            #for item in Basket.all_stuff:
+                #print(item)
 
 
     def add_product(self, product):
@@ -36,24 +40,27 @@ class Basket:
 
 
     def remove_product(self, product):
-        self.base = ('del' + product)
+        self.base = ('rem' + product)
 
 
     def show(self):
         for elem in self.__base:
             print(elem, *Basket.all_stuff[elem])
 
-    @classmethod
-    def price(cls):
-        amount = 0
-        for elem in range(len(cls.all_stuff)):
-            amount += cls.all_stuff[elem][3]
+
+    def price(self):
+        amount, place = 0, 0
+        for item in self.base:
+            if item in self.all_stuff:
+                place = len(self.all_stuff[item]) - 1
+                amount += int(self.all_stuff[item][place])
         print(amount)
 
 
     @classmethod
     def assortment(cls):
         for key in cls.all_stuff:
+            #print(key)
             print(key, *cls.all_stuff[key])
 
 
@@ -94,54 +101,71 @@ class Product:
         return [self.__name, self.__code, self.country, self.__info, self.__price]
 
 
+
 class Shop_managing:
     @staticmethod
     def user():
         basket = Basket()
+        greeting = f'Hi, welcome to our shop!\n'
         interaction = (f'\n'
-                       f'Welcome to our shop!\n'
+                       f'Available actions:\n'
+                       f'To enter the path to the file with data, enter "file"\n'
+                       f'To check this menu again, enter "menu"\n'
                        f'To check available products enter 1\n'
                        f'To add the products into the cart enter 2\n'
                        f'To remove the products from the cart enter 3\n'
-                       f'To see whats currently in the cart enter 4\n'
+                       f'To see what\'s currently in the cart enter 4\n'
                        f'To check the price of the items in the cart enter 5\n'
                        f'To exit enter 0\n')
 
-        print(interaction)
-        request = input()
+        print(greeting, interaction)
+        request = input('Enter your request: ')
 
-        while request != 0:
-            if request == 1:
+
+        while request != '0':
+            if request == 'file':
+                path = input('Enter the path to your file: ')
+                basket.product_info(path)
+                request = input('Enter your request: ')
+
+            elif request.lower() == 'menu':
+                print(interaction)
+                request = input('Enter your request: ')
+                continue
+
+            elif request == '1':
                 basket.assortment()
-                request = input()
+                request = input('Enter your request: ')
 
-            elif request == 2:
-                print('Enter the name of the product u want to add to the cart')
-                item = input()
-                while item not in basket.all_stuff[item]:
-                    print('No such product in the cart')
-                    item = input()
+            elif request == '2':
+                item = input('Enter the name of the product you want to add to the cart: ')
+                if item in Basket.all_stuff:
+                    basket.add_product(item)
+                    print('The product was successfully added!\n')
+                else:
+                    print('No such product in the shop\n', interaction)
+                request = input('Enter your request: ')
 
-                basket.add_product(item)
-                request = input()
+            elif request == '3':
+                item = input('Enter the name of the product you want to remove to the cart: ')
+                if item in basket.base:
+                    basket.remove_product(item)
+                    print('The product was successfully removed!\n')
+                else:
+                    print('No such product in the cart\n', interaction)
+                request = input('Enter your request: ')
 
-            elif request == 3:
-                print('Enter the name of the product u want to remove from the cart')
-                item = input()
-                while item not in basket.all_stuff[item]:
-                    print('No such product in the cart')
-                    item = input()
-
-                basket.remove_product(item)
-                request = input()
-
-            elif request == 4:
+            elif request == '4':
                 basket.show()
-                request = input()
+                request = input('Enter your request: ')
 
-            elif request == 5:
+            elif request == '5':
                 basket.price()
-                request = input()
+                request = input('Enter your request: ')
 
+        print('Have a nice day!')
+
+#Basket.product_info('production_data.txt')
 
 Shop_managing.user()
+
